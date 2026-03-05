67a550110e79965e80b820255d80c65694e2576b
Andy Jones <andy.jones@digital.education.gov.uk>
2026-03-05 20:32:54 +0000

    Updating build

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default ({ env }) => ({
+  
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-sa',
+        providerOptions: {
+          account: env('AZURE_STORAGE_ACCOUNT'),
+          accountKey: env('AZURE_STORAGE_ACCOUNT_KEY'),
+          serviceBaseURL: env('AZURE_STORAGE_URL'),
+          containerName: env('AZURE_STORAGE_CONTAINER_NAME'),
+          defaultPath: 'uploads',
+          cdnBaseURL: env('AZURE_CDN_URL'), 
+        },
+      },
+    },
+  });
+  
